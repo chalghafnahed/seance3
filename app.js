adb5c32f695c7670237c224a8fb2943971eb856a
chalghafnahed <nahed.chalghaf@gmail.com>
2018-10-20 11:20:49 +0100

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-//let name=o && o.getName();
-//let name = otherName || "default ";
-//let allowed = (age>18) ? "yes" : "no";
-// console.log(typeof 37=== 'number');
+let name=o && o.getName();
+let name = otherName || "default ";
+let allowed = (age>18) ? "yes" : "no";
+ console.log(typeof 37=== 'number');
 let contacts = new Array();
 contacts =[];
-contacts= ['Pierre','Paul','Joe'];
+contacts= ['Pierre','Paul','Joe','coco'];
 contacts.push('loli');
 
 contacts.forEach ((c,index) => { console.log(c) });
